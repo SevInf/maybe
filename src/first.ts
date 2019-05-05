205ecb06b5f80722af496288fd031e5f38d224a0
Sergey Tatarintsev <sergey@tatarintsev.me>
2019-05-05 19:57:28 +0200

    refactor: simplify `first` code

--- src/first.ts
+++ src/first.ts
@@ -1,9 +1,4 @@
 import { Maybe, none } from './maybe';
 export function first<T>(variants: Maybe<T>[]): Maybe<T> {
-    for (const variant of variants) {
-        if (!variant.isNone()) {
-            return variant;
-        }
-    }
-    return none;
+    return variants.find(variant => !variant.isNone()) || none;
 }
